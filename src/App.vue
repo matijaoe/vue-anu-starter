eece0fb4fded2e49728fb7754f4cf6c7db26dbd8
Matija Osrečki <matija.osrecki@gmail.com>
2023-04-08 22:18:31 +0200

    Move Drawer to component

--- src/App.vue
+++ src/App.vue
@@ -3,24 +3,8 @@ const drawerStore = useDrawerStore()
 </script>
 
 <template>
-  <ADrawer
-    v-model="drawerStore.isDrawerShown"
-    title="Drawer title"
-    subtitle="Chocolate cake tiramisu donut"
-  >
-    <template #default>
-      <div class="a-card-body">
-        <ABtn
-          variant="light"
-          class="text-sm"
-          @click="drawerStore.close"
-        >
-          Close
-        </ABtn>
-      </div>
-    </template>
-  </ADrawer>
 
+<TheDrawer />
   <main
     container mx-auto p="x-4 y-12" min-h-screen
     text="center base-700 dark:base-200" font-sans
---
+++ src/components/TheDrawer.vue
@@ -0,0 +1,19 @@
+<template>
+  <ADrawer
+    v-model="drawerStore.isDrawerShown"
+    title="Drawer title"
+    subtitle="Chocolate cake tiramisu donut"
+  >
+    <template #default>
+      <div class="a-card-body">
+        <ABtn
+          variant="light"
+          class="text-sm"
+          @click="drawerStore.close"
+        >
+          Close
+        </ABtn>
+      </div>
+    </template>
+  </ADrawer>
+</template>
